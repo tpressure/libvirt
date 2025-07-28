f9c7390a76f94f97c5e433faa5a48cacf5158166
Thomas Prescher <thomas.prescher@cyberus-technology.de>
2025-07-28 15:37:52 +0200

    ch: support serial number for block devices
    virtio-blk supports only 20 characters for the serial number. If a
    serial number exceeds the 20 character limit, truncate it but keep
    the original in the XML.
    
    On-behalf-of: SAP thomas.prescher@sap.com
    Signed-off-by: Thomas Prescher <thomas.prescher@cyberus-technology.de>

--- src/ch/ch_monitor.c
+++ src/ch/ch_monitor.c
@@ -43,6 +43,8 @@
 
 #define VIR_FROM_THIS VIR_FROM_CH
 
+#define VIRTIO_BLK_ID_BYTES 20
+
 VIR_LOG_INIT("ch.ch_monitor");
 
 static virClass *virCHMonitorClass;
@@ -453,6 +455,15 @@ virCHMonitorBuildDiskJson(virJSONValue *disks, virDomainDiskDef *diskdef)
             return -1;
         if (virJSONValueObjectAppendString(disk, "path", diskdef->src->path) < 0)
             return -1;
+
+        if (diskdef->serial) {
+            char serial_buf[VIRTIO_BLK_ID_BYTES + 1];
+            memset(serial_buf, 0, VIRTIO_BLK_ID_BYTES + 1);
+            strncpy(&serial_buf[0], diskdef->serial, VIRTIO_BLK_ID_BYTES);
+            if (virJSONValueObjectAppendString(disk, "serial", serial_buf) < 0)
+                return -1;
+        }
+
         if (diskdef->src->readonly) {
             if (virJSONValueObjectAppendBoolean(disk, "readonly", true) < 0)
                 return -1;
