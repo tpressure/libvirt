9a9c61de935e6d62736e8d6b40d8e228899470ef
Thomas Prescher <thomas.prescher@cyberus-technology.de>
2026-01-06 10:56:50 +0100

    Revert "ch: Drop deadcode from chDomainDetachDeviceLive()"
    This reverts commit 7b70c1868c0fce43539356e661923182e5d2fa0a.
    
    On-behalf-of: SAP thomas.prescher@sap.com
    Signed-off-by: Thomas Prescher <thomas.prescher@cyberus-technology.de>

--- src/ch/ch_hotplug.c
+++ src/ch/ch_hotplug.c
@@ -263,6 +263,7 @@ chDomainDetachDeviceLive(virDomainObj *vm,
     virDomainDeviceDef detach = { .type = match->type };
     virDomainDeviceInfo *info = NULL;
     virCHDomainObjPrivate *priv = vm->privateData;
+    int idx = 0;
 
     switch (match->type) {
     case VIR_DOMAIN_DEVICE_DISK:
@@ -337,6 +338,13 @@ chDomainDetachDeviceLive(virDomainObj *vm,
 
     if (chDomainRemoveDevice(vm, &detach) < 0)
         return -1;
+
+    if (match->type == VIR_DOMAIN_DEVICE_DISK) {
+        idx = chFindDiskId(vm->def, match->data.disk->dst);
+        if (idx >= 0) {
+            virDomainDiskRemove(vm->def, idx);
+        }
+    }
 
     return 0;
 }
