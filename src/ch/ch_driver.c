5bb5b9f8978384bcb8c88d54096a43bb493000b1
Leander Kohler <leander.kohler@cyberus-technology.de>
2026-02-05 13:34:27 +0100

    ch: add additional SMBIOS fields
    + adds support for mode host and sysinfo
    + adds support for fields
      - system-manufacturer
      - sytem-product-name
      - system-version
      - system-serial-number
      - system-uuid
      - system-sku_number
      - system-family
      - chassis-asset-tag
      passed to CH using --platform
    
    On-behalf-of: SAP leander.kohler@sap.com
    Signed-off-by: Leander Kohler <leander.kohler@cyberus-technology.de>

--- src/ch/ch_driver.c
+++ src/ch/ch_driver.c
@@ -1638,6 +1638,7 @@ static int chStateCleanup(void)
         return -1;
 
     virBitmapFree(ch_driver->chCaps);
+    virSysinfoDefFree(ch_driver->hostsysinfo);
     virObjectUnref(ch_driver->config);
     virObjectUnref(ch_driver->xmlopt);
     virObjectUnref(ch_driver->caps);
@@ -1771,6 +1772,9 @@ chStateInitialize(bool privileged,
 
     if (!(ch_driver->xmlopt = chDomainXMLConfInit(ch_driver)))
         goto cleanup;
+
+    if (privileged)
+        ch_driver->hostsysinfo = virSysinfoRead();
 
     if (!(ch_driver->config = virCHDriverConfigNew(privileged)))
         goto cleanup;
