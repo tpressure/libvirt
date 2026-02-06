183c13cc6150a7db6505966135c0ed119175c94b
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-02-06 13:29:58 +0100

    ch: streamline usage of monitor lock guard
    We now use the scoped lock guard for all accesses to the monitor's CURL
    handle. This streamlines all locking of that object in that file.
    
    On-behalf-of: SAP philipp.schuster@sap.com
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>

--- src/ch/ch_monitor.c
+++ src/ch/ch_monitor.c
@@ -1285,7 +1285,6 @@ virCHMonitorPut(virCHMonitor *mon,
                 domainLogContext *logCtxt,
                 virJSONValue **answer)
 {
-    VIR_LOCK_GUARD lock = virObjectLockGuard(mon);
     g_autofree char *url = NULL;
     g_autofree char *payload_str = NULL;
     int responseCode = 0;
@@ -1295,29 +1294,31 @@ virCHMonitorPut(virCHMonitor *mon,
 
     url = g_strdup_printf("%s/%s", URL_ROOT, endpoint);
 
-    /* reset all options of a libcurl session handle at first */
-    curl_easy_reset(mon->handle);
+    VIR_WITH_OBJECT_LOCK_GUARD(mon) {
+        /* reset all options of a libcurl session handle at first */
+        curl_easy_reset(mon->handle);
 
-    curl_easy_setopt(mon->handle, CURLOPT_UNIX_SOCKET_PATH, mon->socketpath);
-    curl_easy_setopt(mon->handle, CURLOPT_URL, url);
-    curl_easy_setopt(mon->handle, CURLOPT_UPLOAD, 1L);
-    curl_easy_setopt(mon->handle, CURLOPT_HTTPHEADER, NULL);
-    curl_easy_setopt(mon->handle, CURLOPT_INFILESIZE, 0L);
+        curl_easy_setopt(mon->handle, CURLOPT_UNIX_SOCKET_PATH, mon->socketpath);
+        curl_easy_setopt(mon->handle, CURLOPT_URL, url);
+        curl_easy_setopt(mon->handle, CURLOPT_UPLOAD, 1L);
+        curl_easy_setopt(mon->handle, CURLOPT_HTTPHEADER, NULL);
+        curl_easy_setopt(mon->handle, CURLOPT_INFILESIZE, 0L);
 
-    headers = curl_slist_append(headers, "Accept: application/json");
+        headers = curl_slist_append(headers, "Accept: application/json");
+        curl_easy_setopt(mon->handle, CURLOPT_HTTPHEADER, headers);
+        curl_easy_setopt(mon->handle, CURLOPT_WRITEFUNCTION, curl_callback);
+        curl_easy_setopt(mon->handle, CURLOPT_WRITEDATA, (void *)&data);
 
-    curl_easy_setopt(mon->handle, CURLOPT_HTTPHEADER, headers);
-    curl_easy_setopt(mon->handle, CURLOPT_WRITEFUNCTION, curl_callback);
-    curl_easy_setopt(mon->handle, CURLOPT_WRITEDATA, (void *)&data);
+        if (payload) {
+            payload_str = virJSONValueToString(payload, false);
+            curl_easy_setopt(mon->handle, CURLOPT_POSTFIELDS, payload_str);
+            curl_easy_setopt(mon->handle, CURLOPT_CUSTOMREQUEST, "PUT");
+            headers = curl_slist_append(headers, "Content-Type: application/json");
+        }
 
-    if (payload) {
-        payload_str = virJSONValueToString(payload, false);
-        curl_easy_setopt(mon->handle, CURLOPT_POSTFIELDS, payload_str);
-        curl_easy_setopt(mon->handle, CURLOPT_CUSTOMREQUEST, "PUT");
-        headers = curl_slist_append(headers, "Content-Type: application/json");
+        responseCode = virCHMonitorCurlPerform(mon->handle);
+        curl_easy_reset(mon->handle);
     }
-    responseCode = virCHMonitorCurlPerform(mon->handle);
-    curl_easy_reset(mon->handle);
 
     data.content = g_realloc(data.content, data.size + 1);
     data.content[data.size] = '\0';
@@ -1362,7 +1363,6 @@ bool
 virCHMonitorPutNoResponse(virCHMonitor *mon, const char *endpoint,
                 const char *payload, domainLogContext *logCtxt)
 {
-    VIR_LOCK_GUARD lock = virObjectLockGuard(mon);
     g_autofree char *url = NULL;
     int responseCode = 0;
     struct curl_data data = {0};
@@ -1370,25 +1370,27 @@ virCHMonitorPutNoResponse(virCHMonitor *mon, const char *endpoint,
 
     url = g_strdup_printf("%s/%s", URL_ROOT, endpoint);
 
-    /* reset all options of a libcurl session handle at first */
-    curl_easy_reset(mon->handle);
+    VIR_WITH_OBJECT_LOCK_GUARD(mon) {
+        /* reset all options of a libcurl session handle at first */
+        curl_easy_reset(mon->handle);
 
-    curl_easy_setopt(mon->handle, CURLOPT_UNIX_SOCKET_PATH, mon->socketpath);
-    curl_easy_setopt(mon->handle, CURLOPT_URL, url);
-    curl_easy_setopt(mon->handle, CURLOPT_UPLOAD, 1L);
-    curl_easy_setopt(mon->handle, CURLOPT_HTTPHEADER, NULL);
-    curl_easy_setopt(mon->handle, CURLOPT_INFILESIZE, 0L);
+        curl_easy_setopt(mon->handle, CURLOPT_UNIX_SOCKET_PATH, mon->socketpath);
+        curl_easy_setopt(mon->handle, CURLOPT_URL, url);
+        curl_easy_setopt(mon->handle, CURLOPT_UPLOAD, 1L);
+        curl_easy_setopt(mon->handle, CURLOPT_HTTPHEADER, NULL);
+        curl_easy_setopt(mon->handle, CURLOPT_INFILESIZE, 0L);
 
-    headers = curl_slist_append(headers, "Accept: application/json");
-    headers = curl_slist_append(headers, "Content-Type: application/json");
-    curl_easy_setopt(mon->handle, CURLOPT_CUSTOMREQUEST, "PUT");
-    curl_easy_setopt(mon->handle, CURLOPT_HTTPHEADER, headers);
-    curl_easy_setopt(mon->handle, CURLOPT_POSTFIELDS, payload);
-    curl_easy_setopt(mon->handle, CURLOPT_WRITEFUNCTION, curl_callback);
-    curl_easy_setopt(mon->handle, CURLOPT_WRITEDATA, (void *)&data);
+        headers = curl_slist_append(headers, "Accept: application/json");
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+        curl_easy_setopt(mon->handle, CURLOPT_CUSTOMREQUEST, "PUT");
+        curl_easy_setopt(mon->handle, CURLOPT_HTTPHEADER, headers);
+        curl_easy_setopt(mon->handle, CURLOPT_POSTFIELDS, payload);
+        curl_easy_setopt(mon->handle, CURLOPT_WRITEFUNCTION, curl_callback);
+        curl_easy_setopt(mon->handle, CURLOPT_WRITEDATA, (void *)&data);
 
-    responseCode = virCHMonitorCurlPerform(mon->handle);
-    curl_easy_reset(mon->handle);
+        responseCode = virCHMonitorCurlPerform(mon->handle);
+        curl_easy_reset(mon->handle);
+    }
 
     data.content = g_realloc(data.content, data.size + 1);
     data.content[data.size] = 0;
@@ -1786,7 +1788,6 @@ int virCHMonitorMigrationSend(virCHMonitor *mon,
 
 retry:
     VIR_WITH_OBJECT_LOCK_GUARD(mon) {
-
         /* See qemuDomainObjEnterMonitorAsync for how qemu handles unlocking the
          * VM in case of migration. */
         virObjectUnlock(mon->vm);
