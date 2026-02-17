31f63bd9f1e8b8c86c9e4490b071fb68bbbb0677
Thomas Prescher <thomas.prescher@cyberus-technology.de>
2026-02-17 14:03:18 +0100

    xxx: unlocked vm

--- src/ch/ch_driver.c
+++ src/ch/ch_driver.c
@@ -2881,11 +2881,11 @@ error:
     VIR_FREE(priv->args);
 
     virDomainObjEndAsyncJob(vm);
-    if (!virDomainObjIsActive(vm)) {
-        DBG("xxxxxxxxxxxxxxx");
-        virCHDomainRemoveInactive(driver, vm);
-    }
-    virDomainObjEndAPI(&vm);
+
+    DBG("xxxxxxxxxxxxxxx");
+    virCHDomainRemoveInactive(driver, vm);
+
+    /* virDomainObjEndAPI(&vm); */
     DBG("Leaving local cleanup");
 }
 
