adce5d8d213cd025c36a93f7faa3ef183fd896c5
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-02-23 14:39:54 +0100

    ch: fix code warning (unused attribute)
    On-behalf-of: SAP philipp.schuster@sap.com
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>

--- src/ch/ch_monitor.c
+++ src/ch/ch_monitor.c
@@ -811,7 +811,7 @@ virCHMonitorBuildDevicesJson(virJSONValue *content,
 }
 
 static int
-virCHMonitorEnsurePlatform(g_autoptr(virJSONValue) *platform)
+virCHMonitorEnsurePlatform(virJSONValue **platform)
 {
     if (!*platform)
         *platform = virJSONValueNewObject();
