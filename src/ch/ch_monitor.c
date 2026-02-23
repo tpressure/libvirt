02e1e11a7fca9c691c061b8da94d8e6fdffa0603
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-02-23 17:09:29 +0100

    ch: monitor: add common virCHMonitorRequest() helper
    This is necessary cleanup to remove code duplication all over the
    place. The motivator for this was that I need a
    "virCHMonitorPutNoContentNoResponse()" but it doesn't exist so far.
    
    So, this is a pre-requisite for adding the new migration cancel call.
    
    On-behalf-of: SAP philipp.schuster@sap.com
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>

--- src/ch/ch_monitor.c
+++ src/ch/ch_monitor.c
@@ -1422,6 +1422,66 @@ curl_callback(void *contents, size_t size, size_t nmemb, void *userp)
     return content_size;
 }
 
+typedef struct _HttpResponse {
+    int code;
+    virJSONValue *json;
+} HttpResponse;
+
+static HttpResponse
+virCHMonitorRequest(virCHMonitor *mon,
+                    const char *endpoint,
+                    const char *payload,
+                    const char *method,
+                    bool parse_payload_as_json)
+{
+    g_autofree char *url = NULL;
+    virJSONValue *retJson = NULL;
+    int responseCode = 0;
+    struct curl_data data = {0};
+    struct curl_slist *headers = NULL;
+    CURL *handle = NULL;
+
+    url = g_strdup_printf("%s/%s", URL_ROOT, endpoint);
+
+    VIR_WITH_OBJECT_LOCK_GUARD(mon) {
+        handle = curl_easy_init();
+        curl_easy_setopt(handle, CURLOPT_UNIX_SOCKET_PATH, mon->socketpath);
+        curl_easy_setopt(handle, CURLOPT_URL, url);
+        curl_easy_setopt(handle, CURLOPT_UPLOAD, 1L);
+        curl_easy_setopt(handle, CURLOPT_HTTPHEADER, NULL);
+        curl_easy_setopt(handle, CURLOPT_INFILESIZE, 0L);
+        headers = curl_slist_append(headers, "Accept: application/json");
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+        curl_easy_setopt(handle, CURLOPT_CUSTOMREQUEST, method);
+        curl_easy_setopt(handle, CURLOPT_HTTPHEADER, headers);
+        if (payload)
+            curl_easy_setopt(handle, CURLOPT_POSTFIELDS, payload);
+        curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, curl_callback);
+        curl_easy_setopt(handle, CURLOPT_WRITEDATA, (void *)&data);
+        responseCode = virCHMonitorCurlPerform(handle);
+        curl_easy_cleanup(handle);
+    }
+
+    data.content = g_realloc(data.content, data.size + 1);
+    data.content[data.size] = 0;
+
+    if (parse_payload_as_json && data.size != 0)
+        retJson = virJSONValueFromString(data.content);
+
+    DBG("HTTP request was: %s /%s", method, endpoint);
+    DBG("HTTP response code from CH: %d", responseCode);
+    if (data.size)
+        DBG("Response = %s", data.content);
+
+    curl_slist_free_all(headers);
+    g_free(data.content);
+
+    return (HttpResponse) {
+        .code = responseCode,
+        .json = retJson,
+    };
+}
+
 static int
 virCHMonitorPut(virCHMonitor *mon,
                 const char *endpoint,
@@ -1550,7 +1610,6 @@ virCHMonitorPutNoResponse(virCHMonitor *mon, const char *endpoint,
     g_free(data.content);
 
     return responseCode == 200 || responseCode == 204;
-}
 }
 
 static int
