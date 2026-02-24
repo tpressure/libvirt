f98366ffc22129ab3127b9a6fa27cc32b1690cc9
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-02-24 13:40:26 +0100

    ch: monitor: fix in chMonitorJSONGetMigrationStatsReply()
    On-behalf-of: SAP philipp.schuster@sap.com
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>

--- src/ch/ch_monitor.c
+++ src/ch/ch_monitor.c
@@ -2299,7 +2299,7 @@ int
 chMonitorJSONGetMigrationStatsReply(virCHMonitor *mon,
                                     chMigrationProgress *progress)
 {
-    g_autoptr(virJSONValue) response;
+    g_autoptr(virJSONValue) response = NULL;
 
     // Unlocked access as the migration might still be going on.
     if (virCHMonitorGet(mon, URL_VM_MIGRATION_PROGRESS, &response) != 0) {
