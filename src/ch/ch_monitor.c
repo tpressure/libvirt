667a0ec436b663c2bced60dc0b00efc7a9178394
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-02-24 13:38:48 +0100

    ch: monitor: use virCHMonitorRequest() in virCHMonitorPut()
    On-behalf-of: SAP philipp.schuster@sap.com
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>

--- src/ch/ch_monitor.c
+++ src/ch/ch_monitor.c
@@ -1473,59 +1473,17 @@ virCHMonitorPutNoResponse(virCHMonitor *mon, const char *endpoint,
 
 virJSONValue*
 virCHMonitorPut(virCHMonitor *mon, const char *endpoint,
-                const char *payload, domainLogContext *logCtxt)
+                const char *payload, G_GNUC_UNUSED domainLogContext *logCtxt)
 {
-    g_autofree char *url = NULL;
-    virJSONValue *retJson = NULL;
-    int responseCode = 0;
-    struct curl_data data = {0};
-    struct curl_slist *headers = NULL;
-    CURL *handle = NULL;
+    HttpResponse response = virCHMonitorRequest(mon, endpoint, payload, "PUT", true);
 
-    url = g_strdup_printf("%s/%s", URL_ROOT, endpoint);
-
-    VIR_WITH_OBJECT_LOCK_GUARD(mon) {
-        handle = curl_easy_init();
-        curl_easy_setopt(handle, CURLOPT_UNIX_SOCKET_PATH, mon->socketpath);
-        curl_easy_setopt(handle, CURLOPT_URL, url);
-        curl_easy_setopt(handle, CURLOPT_UPLOAD, 1L);
-        curl_easy_setopt(handle, CURLOPT_HTTPHEADER, NULL);
-        curl_easy_setopt(handle, CURLOPT_INFILESIZE, 0L);
-        headers = curl_slist_append(headers, "Accept: application/json");
-        headers = curl_slist_append(headers, "Content-Type: application/json");
-        curl_easy_setopt(handle, CURLOPT_CUSTOMREQUEST, "PUT");
-        curl_easy_setopt(handle, CURLOPT_HTTPHEADER, headers);
-        curl_easy_setopt(handle, CURLOPT_POSTFIELDS, payload);
-        curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, curl_callback);
-        curl_easy_setopt(handle, CURLOPT_WRITEDATA, (void *)&data);
-        responseCode = virCHMonitorCurlPerform(handle);
-        curl_easy_cleanup(handle);
+    if (response.code == 200 || response.code == 204) {
+        return g_steal_pointer(&response.json);
     }
 
-    data.content = g_realloc(data.content, data.size + 1);
-    data.content[data.size] = 0;
+    virJSONValueFree(response.json);
 
-    DBG("Reponse code from CH: %d", responseCode);
-
-    if (data.size) {
-        DBG("HTTP Response: %s", data.content);
-    }
-
-    retJson = virJSONValueFromString(data.content);
-
-    if (logCtxt && data.size) {
-        /* Do this to append a NULL char at the end of data */
-        domainLogContextWrite(logCtxt, "HTTP response code from CH: %d\n", responseCode);
-        domainLogContextWrite(logCtxt, "Response = %s\n", data.content);
-    }
-
-    curl_slist_free_all(headers);
-    g_free(data.content);
-
-    if (responseCode != 200 && responseCode != 204)
-        return NULL;
-
-    return retJson;
+    return NULL;
 }
 
 static int
