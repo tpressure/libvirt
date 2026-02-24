1f59d2080848b890415a1847a9882cf6ea5d5b51
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-02-24 13:46:39 +0100

    ch: monitor: remove unneeded virCHMonitorPutNoContent() helper
    On-behalf-of: SAP philipp.schuster@sap.com
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>

--- src/ch/ch_monitor.c
+++ src/ch/ch_monitor.c
@@ -92,7 +92,6 @@ chMonitorSocketConnect(virCHMonitor *mon);
 VIR_ONCE_GLOBAL_INIT(virCHMonitor);
 
 int virCHMonitorShutdownVMM(virCHMonitor *mon);
-int virCHMonitorPutNoContent(virCHMonitor *mon, const char *endpoint);
 static int
 virCHMonitorPut(virCHMonitor *mon,
                 const char *endpoint,
@@ -1518,13 +1517,6 @@ virCHMonitorPut(virCHMonitor *mon,
     return 0;
 }
 
-virCHMonitorPutNoContent(virCHMonitor *mon, const char *endpoint)
-{
-    int responseCode = virCHMonitorRequest(mon, endpoint, NULL, "PUT", false).code;
-
-    return !(responseCode == 200 || responseCode == 204);
-}
-
 bool
 virCHMonitorPutNoResponse(virCHMonitor *mon, const char *endpoint,
                           const char *payload)
@@ -1642,7 +1634,7 @@ virCHMonitorGetThreadInfo(virCHMonitor *mon,
 int
 virCHMonitorShutdownVMM(virCHMonitor *mon)
 {
-    return virCHMonitorPutNoContent(mon, URL_VMM_SHUTDOWN);
+    return virCHMonitorPutNoResponse(mon, URL_VMM_SHUTDOWN, NULL);
 }
 
 int
@@ -1662,31 +1654,31 @@ virCHMonitorCreateVM(virCHDriver *driver, virCHMonitor *mon)
 int
 virCHMonitorBootVM(virCHMonitor *mon)
 {
-    return virCHMonitorPutNoContent(mon, URL_VM_BOOT);
+    return virCHMonitorPutNoResponse(mon, URL_VM_BOOT, NULL);
 }
 
 int
 virCHMonitorShutdownVM(virCHMonitor *mon)
 {
-    return virCHMonitorPutNoContent(mon, URL_VM_SHUTDOWN);
+    return virCHMonitorPutNoResponse(mon, URL_VM_SHUTDOWN, NULL);
 }
 
 int
 virCHMonitorRebootVM(virCHMonitor *mon)
 {
-    return virCHMonitorPutNoContent(mon, URL_VM_REBOOT);
+    return virCHMonitorPutNoResponse(mon, URL_VM_REBOOT, NULL);
 }
 
 int
 virCHMonitorSuspendVM(virCHMonitor *mon)
 {
-    return virCHMonitorPutNoContent(mon, URL_VM_Suspend);
+    return virCHMonitorPutNoResponse(mon, URL_VM_Suspend, NULL);
 }
 
 int
 virCHMonitorResumeVM(virCHMonitor *mon)
 {
-    return virCHMonitorPutNoContent(mon, URL_VM_RESUME);
+    return virCHMonitorPutNoResponse(mon, URL_VM_RESUME, NULL);
 }
 
 int
