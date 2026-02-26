d68e7c44245eee58e6f0cfa0a425b139bc6470a8
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-02-26 10:00:05 +0100

    ch: fix FD leak in virCHMonitorReattach()
    The FD is not used anymore, and we are leaking an FD this way.
    
    The more interesting question is why this FD is unused in the first
    place, but this should be addressed in a follow-up (if necessary).
    
    On-behalf-of: SAP philipp.schuster@sap.com
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>

--- src/ch/ch_monitor.c
+++ src/ch/ch_monitor.c
@@ -1058,9 +1058,8 @@ virCHMonitorReattach(virDomainObj *vm, virCHDriverConfig *cfg, virCHDriver *driv
     // virCHDomainObjPrivate *priv = vm->privateData;
     g_autoptr(virCHMonitor) mon = NULL;
     g_autoptr(virCommand) cmd = NULL;
-    int socket_fd = 0;
-    int event_monitor_fd;
-    // int rv;
+    VIR_AUTOCLOSE socket_fd = -1;
+    int event_monitor_fd = 0;
 
     if (virCHMonitorInitialize() < 0)
         return NULL;
