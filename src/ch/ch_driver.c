3f1db32cc695fba5c51ff865a67207367b3814ef
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2026-03-03 14:17:09 +0100

    ch: context for more error paths
    On-behalf-of: SAP philipp.schuster@sap.com
    Signed-off-by: Philipp Schuster <philipp.schuster@cyberus-technology.de>

--- src/ch/ch_driver.c
+++ src/ch/ch_driver.c
@@ -3122,6 +3122,8 @@ chDomainMigratePrepare3(virConnectPtr dconn,
     }
 
     if (!(def = chMigrationAnyPrepareDef(driver, dom_xml, dname))) {
+        virReportError(VIR_ERR_OPERATION_FAILED, "%s",
+                       _("Failed to prepare domain def"));
         rc = -1;
         goto err_cleanup_args;
     }
@@ -3168,6 +3170,8 @@ chDomainMigratePrepare3(virConnectPtr dconn,
     }
 
     if (virCHProcessInit(driver, vm) < 0) {
+        virReportError(VIR_ERR_OPERATION_FAILED, "%s",
+                       _("Failed to init cloud-hypervisor process"));
         DBG("Could not init process");
         rc = -1;
         goto err_cleanup_job_start;
@@ -3211,6 +3215,8 @@ chDomainMigratePrepare3(virConnectPtr dconn,
     if (vm->def->numa) {
         args->cells = virJSONValueNewObject();
         if (virCHMonitorBuildMemoryZonesJson(args->cells, vm->def) != 0) {
+            virReportError(VIR_ERR_OPERATION_FAILED, "%s",
+                           _("Failed to process NUMA info"));
             DBG("failed to process numa info");
             rc = -1;
             goto err_cleanup_cells;
