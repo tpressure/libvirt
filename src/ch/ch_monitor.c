020d195bc95c5e06d8efce080c23ab21e9230723
Thomas Prescher <thomas.prescher@cyberus-technology.de>
2026-03-17 20:49:12 +0100

    ch: fix return code for virCHMonitorMigrationCancel
    On-behalf-of: SAP thomas.prescher@sap.com
    Signed-off-by: Thomas Prescher <thomas.prescher@cyberus-technology.de>

--- src/ch/ch_monitor.c
+++ src/ch/ch_monitor.c
@@ -2348,5 +2348,5 @@ int
 virCHMonitorMigrationCancel(virCHMonitor *mon) {
     int responseCode = virCHMonitorRequest(mon, URL_VM_MIGRATION_CANCEL, NULL, "PUT", false, true).code;
 
-    return !(responseCode == 200 || responseCode == 204);
+    return (responseCode == 200 || responseCode == 204) ? 0 : -1;
 }
